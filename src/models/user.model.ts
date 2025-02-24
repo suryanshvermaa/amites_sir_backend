b296e01e54aa19590163f315ff14ed0e54c0aae1
suryanshvermaa <154125921+suryanshvermaa@users.noreply.github.com>
2025-02-24 20:05:28 +0530

    user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,50 @@
 import mongoose,{Document,Schema} from 'mongoose';
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document{
-    
+    name:string;
+    email:string;
+    password:string;
+    comparePassword:(password:string)=>Promise<boolean>;
+    isAdmin:boolean;
+    refreshToken:string;
 }
 
 const UserSchema:Schema<IUser>=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false
+    },
+    isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    refreshToken:{
+        type:String,
+        required:true
+    },
+},{timestamps:true})
 
+UserSchema.pre<IUser>('save',async function(next){
+    if(!this.isModified('password')) next();
+    this.password=await bcrypt.hash(this.password,10);
+    next();
 })
+
+UserSchema.methods.comparePassword=async function(enteredPassword:string){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 
 const User=mongoose.model('User',UserSchema)
 export default User;
